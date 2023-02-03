22981f2c139bb408968d643bc04f7bcc976146b5
zinaric <zinaric@gmail.com>
2023-02-03 18:49:29 +0100

    postavljeni glavni dijelovi prve stranice

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log('ADRIATIC COASTING');
+
+const artistButtons = document.querySelectorAll('.artist-name');
+const artistBoxes = document.querySelectorAll('.artist-info');
+
+artistBoxes.forEach((box) => {
+    box.style.display = 'none';
+})
+
+artistButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        const toHide = button.nextElementSibling;
+        if (toHide.style.display == 'none') {
+            artistBoxes.forEach((box) => {
+                box.style.display = 'none';
+            })
+            toHide.style.display = 'block';
+        } else {
+            toHide.style.display = 'none';
+        }
+
+    })
+})
+///////////////////
+
+const prozor = document.querySelector('body');
+const sunce = document.getElementById('sunce');
+let sunceRight = -1;
+let sunceDown = -1;
+prozor.onscroll = () => {
+    console.log('ines')
+    sunceRight = sunceRight + 1;
+    sunceDown = sunceDown + 3;
+    console.log(sunceRight);
+    sunce.style.left = `${260 + sunceRight}px`
+    sunce.style.top =   `${250 + sunceDown}px`
+}
+
+
+let oldValue = 0;
+window.addEventListener('scroll', function(e){
+
+    // Get the new Value
+    newValue = window.pageYOffset;
+    console.log(newValue);
+
+    //Subtract the two and conclude
+    if(oldValue - newValue > 0){
+        console.log("Up");
+    } else if(oldValue - newValue < 0){
+        console.log("Down");
+    }
+
+    // Update the old value
+    oldValue = newValue;
+});
+
+let scroll_position = 0;
+let scroll_direction;
+
+window.addEventListener('scroll', function(){
+    scroll_direction = (document.body.getBoundingClientRect()).top > scroll_position ? 'up' : 'down';
+    scroll_position = (document.body.getBoundingClientRect()).top;
+    console.log('zadnje' + scroll_direction);
+});
