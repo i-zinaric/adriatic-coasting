039e06f02c7f5a54d0b5f55e065d2e1de8d11e8b
zinaric <zinaric@gmail.com>
2023-02-07 19:53:07 +0100

    postavi mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 console.log('ADRIATIC COASTING');
-/////////////////
-///////////////////////////////
+
+
+///GUMBI ZA OTVARANJE MENIJA MOBILE/////
+const openButton = document.querySelector('#open-menu-button');
+const closeButton = document.querySelector('#close-menu-button');
+
+openButton.addEventListener('click', function() {
+    document.querySelector('#closed-menu-header-mobile').style.display = 'none';
+    document.querySelector('header').style.display = 'block';
+    openButton.style.display = 'none';
+    closeButton.style.display = 'inline-block';
+})
+
+closeButton.addEventListener('click', function() {
+    document.querySelector('#closed-menu-header-mobile').style.display = 'block';
+    document.querySelector('header').style.display = 'none';
+    openButton.style.display = 'inline-block';
+    closeButton.style.display = 'none';
+})
+/////////////////////
 
 
 //////GUMBI ZA OTVARANJE ARTIST INFO//////
@@ -28,3 +46,4 @@ artistButtons.forEach((button) => {
     })
 })
 //////////////////
+
