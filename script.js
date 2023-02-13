dbdaa5f06fc9466f952e65220bc49df9e8d46f50
zinaric <zinaric@gmail.com>
2023-02-13 21:58:01 +0100

    podesi contact form

--- script.js
+++ script.js
@@ -39,11 +39,44 @@ artistButtons.forEach((button) => {
             })
             toHide.style.display = 'block';
                 const y = window.pageYOffset + button.getBoundingClientRect().top;
-                window.scrollTo(0, (y - 6));
+                window.scrollTo(0, (y + 50));
         } else {
             toHide.style.display = 'none';
         }
     })
 })
-//////////////////
+
+
+///DISABLE SCROLL//  privremeno rješenje//
+function disableScroll() {
+    // daje trenutnu scroll poziciju
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // na pokušaj skrolanja vraća skrol na poziciju s koje pokušaj kreće
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+        //vraća skrolanje
+function enableScroll() {
+    window.onscroll = function() {};
+}
+//////////////////BOOK NOW BUTTONS///////////
+const contractForm = document.querySelector('#contract-form-container');
+const bookNowButtons = document.querySelectorAll('.book-now ');
+
+
+bookNowButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        disableScroll();
+        contractForm.style.display = 'block';        
+    })
+})
+
+const closeContractForm = document.querySelector('#close-contract-form');
+closeContractForm.addEventListener('click', () => {
+    enableScroll();
+    contractForm.style.display = 'none';
+})
 
